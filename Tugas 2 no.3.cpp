5c0437c0f3ea1e7aff1543bae8069a697ccbab79
sandrina02 <113509781+sandrina02@users.noreply.github.com>
2022-09-22 10:40:07 +0700

    Add files via upload

---
+++ Tugas 2 no.3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float a = 4500.02;
+  int b;
+  cout << "masukan jumlah barang : ";
+  cin >> b;
+  cout << "yang harus di bayar = " << a * b << endl;
+}
